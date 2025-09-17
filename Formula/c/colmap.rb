6661ad9e0384b8b6dc5cb6a91d213918d9a39109
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-17 10:55:11 +0000

    colmap 3.12.6

--- Formula/c/colmap.rb
+++ Formula/c/colmap.rb
@@ -1,8 +1,8 @@
 class Colmap < Formula
   desc "Structure-from-Motion and Multi-View Stereo"
   homepage "https://colmap.github.io/"
-  url "https://github.com/colmap/colmap/archive/refs/tags/3.12.5.tar.gz"
-  sha256 "93dfb220cce24d988506bbb1d27d4278eacfd4e372df61d380559d414c1bd9e4"
+  url "https://github.com/colmap/colmap/archive/refs/tags/3.12.6.tar.gz"
+  sha256 "f66d34be7a738fa753d1b71aec4fb7411d8c117beb58d1f2ba84ee2696c96410"
   license "BSD-3-Clause"
 
   bottle do
